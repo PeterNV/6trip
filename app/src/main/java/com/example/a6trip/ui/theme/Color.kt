ac4372b25193deb39c62c2184f87279e159ec3e4
PedroVilaNova <phpvn@discente.ifpe.edu.br>
2026-02-02 21:24:23 -0300

    COMMIT 1

---
+++ app/src/main/java/com/example/a6trip/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.a6trip.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+val Aqua80 = Color(0xFF00FFFF)
+val White = Color(0xFFFFFFFF)
+val GrayL = Color(0xFFD3D3D3)
+val GrayD = Color(0xFF808080)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val GreenL = Color(0xFF00FF00)
+val Black = Color(0xFF000000)
+val Red = Color(0xFFFF0000)
