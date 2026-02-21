5417b40f86698f85b736455d1d479c4583937b61
allamymp <allamympereira@gmail.com>
2026-02-20 22:34:59 -0300

    REFACTORING: criava um MutableInteractionSource para cada elemento sobrecarregando o processamento. Agora passa a usar apenas um reutilizável.

--- app/src/main/java/com/example/a6trip/MainActivity.kt
+++ app/src/main/java/com/example/a6trip/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.a6trip
 
 import android.os.Bundle
+import android.os.StrictMode
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -15,6 +16,14 @@ import com.example.a6trip.ui.theme._6tripTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build()
+        )
+
         enableEdgeToEdge()
         setContent {
             _6tripTheme {
